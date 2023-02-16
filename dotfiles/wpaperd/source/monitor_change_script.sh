1a53d92d325546a1d4412e47ee2ee4e65e85067f
nguyenston <phucnguyen1999.nn@gmail.com>
2023-02-16 17:18:03 -0500

    wpaperd should now restart when monitors are added/removed

--- dotfiles/wpaperd/source/monitor_change_script.sh
+++ dotfiles/wpaperd/source/monitor_change_script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | grep 'monitor' | while read -r line; do
+socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | stdbuf -o0 grep 'monitor' | while read -r line; do
 	wpaperd
 done
