00f52f337c4875db178b31d6b1ef69b2dc496660
nguyenston <phucnguyen1999.nn@gmail.com>
2024-02-06 16:14:02 -0500

    removed temporary code

--- configuration.nix
+++ configuration.nix
@@ -146,15 +146,6 @@ in
   # enable wake from sleep through usb devices
   services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0083", ATTR{power/wakeup}="enabled"
-
-    # Remove NVIDIA USB xHCI Host Controller devices, if present
-    # ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA USB Type-C UCSI devices, if present
-    # ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA Audio devices, if present
-    # ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA VGA/3D controller devices
-    # ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
   '';
 
   boot.extraModprobeConfig = ''
@@ -163,7 +154,6 @@ in
     options nvidia-drm modeset=1
   '';
   boot.blacklistedKernelModules = [ "nouveau" ];
-  # boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
   
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
