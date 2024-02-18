ed9807d943a717c09abc181f9d0119e3d43a2f3a
nguyenston <phucnguyen1999.nn@gmail.com>
2024-02-18 12:58:11 -0500

    fixed telegram desktop entry

--- desktop-entries/telegram/default.nix
+++ desktop-entries/telegram/default.nix
@@ -1,24 +1,25 @@
 {
-  webcord = {
-    categories = [ "Chat" "Network" "InstantMessaging" "Qt"];
+  "org.telegram.desktop" = {
+    categories = [ "Chat" "Network" "InstantMessaging" "Qt" ];
     comment = "Official desktop version of Telegram messaging app";
-    exec = "QT_QPA_PLATFORM=\"wayland;xcb\" telegram-desktop";
+    exec = "/usr/bin/env bash -c \"QT_QPA_PLATFORM='wayland;xcb' telegram-desktop -- %u\"";
     icon = "telegram";
     name = "Telegram Desktop";
     type = "Application";
     terminal = false;
-    mimeType = "x-scheme-handler/tg";
+    mimeType = [ "x-scheme-handler/tg" ];
     actions = {
       quit = {
-        exec = "QT_QPA_PLATFORM=\"wayland;xcb\" telegram-desktop -quit";
+        exec = "/usr/bin/env bash -c \"QT_QPA_PLATFORM='wayland;xcb' telegram-desktop -quit\"";
         name = "Quit Telegram";
         icon = "application-exit";
       };
     };
     settings = {
-      TryExec = "QT_QPA_PLATFORM=\"wayland;xcb\" telegram-desktop";
+      Categories = "Chat;Network;InstantMessaging;Qt;";
+      TryExec = "telegram-desktop";
       StartupWMClass = "TelegramDesktop";
-      Keywords = "tg;chat;im;messaging;messenger;sms;tdesktop";
+      Keywords = "tg;chat;im;messaging;messenger;sms;tdesktop;";
       DBusActivatable = "true";
       SingleMainWindow = "true";
       X-GNOME-UsesNotifications = "true";
