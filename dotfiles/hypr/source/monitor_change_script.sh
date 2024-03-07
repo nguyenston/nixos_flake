dca40ad2a1e6353bce047c6a4eac97b7f59767f3
nguyenston <phucnguyen1999.nn@gmail.com>
2024-03-07 12:20:15 -0500

    switch to using ags for status bar

--- dotfiles/hypr/source/monitor_change_script.sh
+++ dotfiles/hypr/source/monitor_change_script.sh
@@ -2,6 +2,6 @@
 socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - |
 	stdbuf -o0 grep '^monitor' | stdbuf -o0 grep -v 'HEADLESS' | while read -r line; do
 	pkill wpaperd && wpaperd
-	sleep 2
-	eww open bar_main
+	sleep 1
+	ags -q && nohup ags 0<&- &>/dev/null &
 done
