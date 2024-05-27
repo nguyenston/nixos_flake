f5e349d6f750b4c20528b4eaaafb2bad061aa48e
nguyenston <phucnguyen1999.nn@gmail.com>
2024-05-27 15:52:32 -0400

    fix unyeildy suspend behavior

--- scripts/bin/sshfs-reconnect.sh
+++ scripts/bin/sshfs-reconnect.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-#
-while true; do
-	sshfs -f -o ConnectTimeout=3,ConnectionAttempts=1,ServerAliveInterval=5,ServerAliveCountMax=3 $@
-	echo "disconnected from $@"
-	sleep 3
-	echo "retry $@ ..."
-done
+
+sshfs -f -o ConnectTimeout=3,ConnectionAttempts=1,ServerAliveInterval=5,ServerAliveCountMax=3 $@
+echo "disconnected from $@"
+# while true; do
+# 	sleep 3
+# 	echo "retry $@ ..."
+# done
