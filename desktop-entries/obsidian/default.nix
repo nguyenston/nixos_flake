6b000a32bb02f6b9fe10fa870d531f49834b739c
nguyenston <phucnguyen1999.nn@gmail.com>
2024-07-11 16:39:08 -0400

    fix obsidian wayland desktop entry

--- desktop-entries/obsidian/default.nix
+++ desktop-entries/obsidian/default.nix
@@ -2,7 +2,8 @@
   obsidian_wayland = {
     categories = [ "Office" ];
     comment = "Knowledge base";
-    exec = "/usr/bin/env bash -c \"env LD_LIBRARY_PATH=\\$(nix build --print-out-paths --no-link nixpkgs#libGL)/lib obsidian --ozone-platform-hint=auto %u\"";
+    # exec = "/usr/bin/env bash -c \"env LD_LIBRARY_PATH=\\$(nix build --print-out-paths --no-link nixpkgs#libGL)/lib obsidian --ozone-platform-hint=auto %u\"";
+    exec = "/usr/bin/env bash -c \"obsidian --ozone-platform-hint=auto %u\"";
     icon = "obsidian";
     mimeType = [ "x-scheme-handler/obsidian" ];
     name = "Obsidian Wayland";
