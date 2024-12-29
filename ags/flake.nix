dd77948687875d103baba0b1b7cd571214f449e0
nguyenston <phucnguyen1999.nn@gmail.com>
2024-12-29 18:19:02 -0500

    agsv2 scaffolding

---
+++ ags/flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "My Awesome Desktop Shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    ags = {
+      url = "github:aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ags,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system} = {
+      default = ags.lib.bundle {
+        inherit pkgs;
+        src = ./.;
+        name = "ags2";
+        entry = "app.ts";
+        gtk4 = true;
+
+        # additional libraries and executables to add to gjs' runtime
+        extraPackages = [
+          ags.packages.${system}.battery
+          ags.packages.${system}.hyprland
+          ags.packages.${system}.auth
+          ags.packages.${system}.bluetooth
+          ags.packages.${system}.tray
+          ags.packages.${system}.wireplumber
+          # pkgs.fzf
+        ];
+      };
+    };
+
+    devShells.${system} = {
+      default = pkgs.mkShell {
+        buildInputs = [
+          ags.packages.${system}.agsFull
+        ];
+      };
+    };
+  };
+}
