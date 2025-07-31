27999061cc25594c9554c1820e926654b624ceed
nguyenston <phucnguyen1999.nn@gmail.com>
2025-07-30 20:59:11 -0400

    pin ags version

--- ags/flake.nix
+++ ags/flake.nix
@@ -5,50 +5,53 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
     ags = {
-      url = "github:aylur/ags";
+      url = "github:Aylur/ags/v2.3.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ags,
-  }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    # packages.${system} = {
-    #   default = ags.lib.bundle {
-    #     inherit pkgs;
-    #     src = ./.;
-    #     name = "ags2";
-    #     entry = "app.ts";
-    #     gtk4 = false;
-    #
-    #     # additional libraries and executables to add to gjs' runtime
-    #     extraPackages = [
-    #       ags.packages.${system}.battery
-    #       ags.packages.${system}.hyprland
-    #       ags.packages.${system}.auth
-    #       ags.packages.${system}.bluetooth
-    #       ags.packages.${system}.tray
-    #       ags.packages.${system}.wireplumber
-    #       ags.packages.${system}.network
-    #       ags.packages.${system}.mpris
-    #       ags.packages.${system}.apps
-    #       ags.packages.${system}.notifd
-    #       # ags.packages.${system}.agsFull
-    #     ];
-    #   };
-    # };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ags,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system} = {
+        default = ags.lib.bundle {
+          inherit pkgs;
+          src = ./.;
+          name = "ags2";
+          entry = "app.ts";
+          gtk4 = false;
 
-    devShells.${system} = {
-      default = pkgs.mkShell {
-        buildInputs = [
-          ags.packages.${system}.agsFull
-        ];
+          # additional libraries and executables to add to gjs' runtime
+          extraPackages = [
+            ags.packages.${system}.battery
+            ags.packages.${system}.hyprland
+            ags.packages.${system}.auth
+            ags.packages.${system}.bluetooth
+            ags.packages.${system}.tray
+            ags.packages.${system}.wireplumber
+            ags.packages.${system}.network
+            ags.packages.${system}.mpris
+            ags.packages.${system}.apps
+            ags.packages.${system}.notifd
+            # ags.packages.${system}.agsFull
+          ];
+        };
+      };
+
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = [
+            ags.packages.${system}.agsFull
+          ];
+        };
       };
     };
-  };
 }
